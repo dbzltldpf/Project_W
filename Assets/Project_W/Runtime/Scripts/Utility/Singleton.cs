71a30c613d0373d79d3b189319b6a1725a4984b1
sunini <dbzltldpf@gmail.com>
2025-08-28 15:31:55 +0900

    init

---
+++ Assets/Project_W/Runtime/Scripts/Utility/Singleton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    protected static T _instance;
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindAnyObjectByType<T>();
+                if (_instance == null)
+                {
+                    GameObject obj = new GameObject();
+                    _instance = obj.AddComponent<T>();
+                }
+            }
+            return _instance;
+        }
+    }
+}
